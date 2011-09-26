863ebd02333cc4fdbfa7b19110208f36c3bcf2f0
Jon Pretty <jon@bang-on.net>
2011-09-26 16:27:26 +0100

    Added IpAddress classes and support

---
+++ src/ip.scala
@@ -0,0 +1,25 @@
+package rapture.io
+
+object IpAddress {
+
+  def parse(s : String) = {
+    val vs = s.split("\\.").map(_.toInt)
+    IpAddress(vs(0), vs(1), vs(2), vs(3))
+  }
+  
+  def fromLong(lng : Long) =
+    IpAddress((lng>>24 & 255L).toInt, (lng>>16 & 255L).toInt, (lng>>8 & 255L).toInt,
+        (lng & 255L).toInt)
+  
+  lazy val privateSubnets = List(IpAddress(10, 0, 0, 0), IpAddress(192, 168, 0, 0),
+      IpAddress(172, 16, 0, 0))
+}
+
+case class IpAddress(b1 : Int, b2 : Int, b3 : Int, b4 : Int) {
+  def asLong = (b1.toLong<<24) + (b2<<16) + (b3<<8) + b4
+  def /(i : Int) = IpAddress.fromLong((asLong>>(32 - i))<<(32 - i))
+  def inSubnet(ip : IpAddress) = (asLong & ip.asLong) == ip.asLong
+  override def toString() = b1+"."+b2+"."+b3+"."+b4
+  def isPrivate = IpAddress.privateSubnets.exists(inSubnet)
+}
+
