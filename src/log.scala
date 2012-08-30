bb17053c959cccfe7de1638648e30e2213007735
Jon Pretty <jon@bang-on.net>
2012-08-30 16:48:15 +0100

    Updates to logging

--- src/log.scala
+++ src/log.scala
@@ -1,13 +1,17 @@
 package rapture.io
 
-import scala.collection.immutable.HashMap
-
 /** Basic logging functionality, introducing the concept of logging zones. Note that this is almost
   * certainly not as efficient as it ought to be, so use log4j if efficiency matters to you. */
 
 case class Zone(name: String)
 
 case class Level(level: Int, name: String)
+object Trace extends Level(6, "trace")
+object Debug extends Level(5, "debug")
+object Info extends Level(4, "info")
+object Warn extends Level(3, "warn")
+object Error extends Level(2, "error")
+object Fatal extends Level(1, "fatal")
 
 trait Logger { def log(msg: String) }
 
@@ -25,28 +29,15 @@ object log {
 
   implicit val zone = Zone("logger")
   
-  var listeners: List[(Logger, Map[Zone, Level])] = Nil
+  var listeners: List[(Logger, Level, Map[Zone, Level])] = Nil
 
   val df = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
   var dateString = ""
   var dateCreated = 0L
 
-  object Trace extends Level(6, "trace")
-  object Debug extends Level(5, "debug")
-  object Info extends Level(4, "info")
-  object Warn extends Level(3, "warn")
-  object Error extends Level(2, "error")
-  object Fatal extends Level(1, "fatal")
-
-  def listen(logger: Logger): Unit =
-    listen(logger, Info)
-
-  def listen(logger: Logger, level: Level): Unit =
-    listen(logger, new HashMap[Zone, Level] { override def default(k: Zone) = level })
-
-  def listen(logger: Logger, spec: Map[Zone, Level]): Unit = {
+  def listen(logger: Logger, level: Level = Info, spec: Map[Zone, Level] = Map()): Unit = {
     info("Registering listener")
-    listeners ::= (logger -> spec)
+    listeners = (logger, level, spec) :: listeners
   }
 
   @inline def trace(msg: => String)(implicit zone: Zone) =
@@ -71,12 +62,12 @@ object log {
     log(Error, zone, e.toString)
 
   private def log(level: Level, zone: Zone, msg: String, time: Long = System.currentTimeMillis) = {
+    // Ensures the date is not formatted every time
     if(time != dateCreated) {
       dateString = df.format(time)
       dateCreated = time
     }
-    val formattedMsg = "%1$23s %2$5s %3$7s %4$s\n".format(dateString, level.name, zone.name, msg)
-    for((lgr, spec) <- listeners if spec.getOrElse(zone, Error).level >= level.level) lgr.log(formattedMsg)
+    val formattedMsg = "%1$23s %2$5s %3$10s %4$s\n".format(dateString, level.name, zone.name, msg)
+    for((lgr, level, spec) <- listeners if spec.getOrElse(zone, level).level >= level.level) lgr.log(formattedMsg)
   }
-
 }
