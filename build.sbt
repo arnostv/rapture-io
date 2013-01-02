4e81852ed46a81fb74db63ae0bda311d1d18853a
Jon Pretty <jon@bang-on.net>
2013-01-02 18:15:39 +0000

    Reluctantly added SBT buildfile, and included test framework

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "rapture-io"
+
+version := "0.7.1"
+
+scalaVersion := "2.10.0"
+
+scalaSource in Compile := file("src")
---
+++ src/ansi.scala
@@ -0,0 +1,54 @@
+/**************************************************************************************************
+Rapture Test Library
+Version 0.5.0
+
+The primary distribution site is
+
+  http://rapture.io/
+
+Copyright 2010-2012 Propensive Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions and limitations under the
+License.
+***************************************************************************************************/
+
+package rapture.test
+
+object Ansi {
+  val Esc = 27.toChar
+  val Normal = Esc+"[0m"
+  val Bold = Esc+"[1m"
+  val Underline = Esc+"[4m"
+  val Blink = Esc+"[5m"
+  val Reverse = Esc+"[7m"
+  val Nondisplayed = Esc+"[8m"
+  val Black = Esc+"[30m"
+  val Red = Esc+"[31m"
+  val Green = Esc+"[32m"
+  val Yellow = Esc+"[33m"
+  val Blue = Esc+"[34m"
+  val Magenta = Esc+"[35m"
+  val Cyan = Esc+"[36m"
+  val White = Esc+"[37m"
+  val BgBlack = Esc+"[30m"
+  val BgRed = Esc+"[31m"
+  val BgGreen = Esc+"[32m"
+  val BgYellow = Esc+"[33m"
+  val BgBlue = Esc+"[34m"
+  val BgMagenta = Esc+"[35m"
+  val BgCyan = Esc+"[36m"
+  val BgWhite = Esc+"[37m"
+  def cursor(row : Int, col : Int) = Esc+"["+row+";"+col+"H"
+  def up(n : Int) = Esc+"["+n+"A"
+  def down(n : Int) = Esc+"["+n+"B"
+  def right(n : Int) = Esc+"["+n+"C"
+  def left(n : Int) = Esc+"["+n+"D"
+}
+
